4c7907c688ee1d202cc4e461d1a6be8732245945
rom <raczkaromain@gmail.com>
2012-09-28 17:56:57 +0200

    rajout projet exo sur les fonctions

---
+++ fonctions/fonctions.c
@@ -0,0 +1,116 @@
+/*************************************************************
+*              Fonctions du programme                        *
+*************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/** fonction permuter **/
+void permute(int x, int y)
+{
+    // Variable et constante
+    int temp;
+
+
+    temp = x;
+    x = y;
+    y = temp;
+    printf("Nouvelle valeur de x = %d\n",x);
+    printf("Nouvelle valeur de y = %d\n",y);
+
+
+}
+
+
+
+
+/** fonction entrervalpos **/
+int entreValPos()
+{
+
+    int val;
+
+    do {
+
+    printf(" Entrez une valeur positive : ");
+    scanf("%d",&val);
+
+    } while (val<0);
+
+
+
+    return val;
+
+
+}
+
+
+
+/** fonction somme **/
+int somme(int n)
+{
+    int valSomme=0;
+    int i;
+
+    for (i=n;i>=0;i--)
+    {
+
+        valSomme=valSomme+n-i;
+
+    }
+
+
+    return valSomme;
+
+}
+
+
+
+/** fonction suite **/
+void suite(int n)
+{
+    //varables et constantes
+    int i;
+
+
+    //fonction
+    printf(" La suite s'écrit ");
+    for (i=n;i>=0;i--){
+
+        if (i!=0 && i!=n) printf(" %d +",n-i);
+
+    }
+    printf(" %d = %d",n,somme(n));
+
+
+}
+
+
+
+
+void trierValeur(int x,int y, int z)
+{
+    //Variables & constantes
+    int max;                        //valeur de la plus grande variable
+
+
+    //Début Fonction
+
+
+    if (x>y) {
+        if (x>z){
+            max = x;
+        } else max = z;
+    }
+    else if (y>z) {
+        max = y;
+    } else max = z;
+
+    printf("Plus grande valeur = %d ",max);
+
+
+
+}
+
+
+
---
+++ fonctions/main.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//int main()
+//{
+//    float k,s;
+//
+//
+//    k=1;
+//    s=89;
+//    bonjour(k,s);
+//
+//    return 0;
+//}
+//
+//
+//void bonjour (float p, float q)
+//{
+//    printf("Hello world! voici les caractères %f et %f \n",p,q);
+//}
+
+
+/**************************
+******exerciceI.1*********
+*************************/
+//void permute(int x,int y);
+//
+//int main()
+//{
+//    //Variables
+//    int x;
+//    int y;
+//
+//
+//
+//    //Fonction
+//    printf("Valeur de x = ");
+//    scanf("%d",&x);
+//    printf("Valeur de y = ");
+//    scanf("%d",&y);
+//
+//    permute(x,y);
+//
+//    return 0;
+//}
+
+
+/********************************************************
+**  suite somme et valeur positive avec les fonctions  **
+********************************************************/
+
+//// prototype des fonctions //
+//int entreValPos();                 // fonction pour entrer une valeur positive
+//int somme(int y);                       // fonction pour calculer la somme
+//void suite(int z);                      // fonction pour écrire la suite à l'écran
+//
+//
+//// fonction principale
+//int main()
+//{
+//    int val;
+//
+//
+//    val=entreValPos();
+//    printf(" Somme de la suite = %d \n",somme(val));
+//    suite(val);
+//
+//
+//return 0;
+//
+//}
+
+
+
+/********************************************************
+*                   trier 3 valeurs                     *
+********************************************************/
+
+// prototype des fonctions //
+void trierValeur(int x,int y, int z);
+
+
+
+// fonction principale
+
+int main()
+{
+    int a,b,c;
+
+    printf("valeur de a = ");
+    scanf("%d",&a);
+    printf("valeur de b = ");
+    scanf("%d",&b);
+    printf("valeur de c = ");
+    scanf("%d",&c);
+
+    trierValeur(a,b,c);
+
+    return 0;
+
+}
