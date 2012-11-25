b6446456f4109eb90e415febb8c3f0eec0b5a41b
th3fr33man <raczkaromain@gmail.com>
2012-11-25 14:54:37 +0100

    maj module.java

--- java/Tp1/Module.java
+++ java/Tp1/Module.java
@@ -17,7 +17,7 @@ public class Module {
 	/**
 	 * taille courante du groupe suivant le module
 	 */
-	private int taille=0;
+	private int taille;
 	
 	/**
 	 * nombre max d'étudiant pour le module
@@ -31,24 +31,35 @@ public class Module {
 	 */
 	public Module(String tit, String sig, int tmax){
 		// ....  a completer
-		listeEtu = new Etudiant[tmax];// créer le tableau ( VIDE ) d'étudiants de taille_max case 
+		
 		titre = tit;
 		sigle = sig;
+		taille = 0;
+		taille_max=tmax;
+		listeEtu = new Etudiant[taille_max];// créer le tableau ( VIDE ) d'étudiants de taille_max case 
+
 	}
 	
 	public boolean ajoute(Etudiant e){
+		boolean val=true;
+	
+		for (int i=0;i<taille;i++){		// boucle for pour vérifier que l'Etudiant n'est pas déjà présent dans le module.
+			if (listeEtu[i]==e){
+			val=false;
+			} 
+		}
 		
-		if (					// ici condition si le champ est vide renvoi vrai
+		if (taille<=taille_max && val==true){				// ici condition si le champ est vide renvoi vrai
 		listeEtu[taille] = e;
 		taille++;
-		
-		return true;
-		} else return false;
+		val=true;
+		} else val=false;
 	
+	return val;
 	}
 	
 	public int getNbEtudiant(){
-		return taille;
+		return taille+1;
 	}	
 	
 	public String getSigle(){
@@ -60,9 +71,12 @@ public class Module {
 	}
 	
 	public String toString(){
-		for (int i; i<taille; i++){		// A voir pour boucle for
-		String res = this.titre() + this.sigle();
+
+		String res = this.getTitre() + " - " + this.getSigle() + "\n";
+		for (int i=0; i<taille; i++){			// Affichage de tous les étuduants du module
+			res += " " + listeEtu[i] + "\n";	//.toString();
 		}
+		return res;
 	}
 	
 	public static void main(String [] args){
@@ -76,7 +90,6 @@ public class Module {
 		m14.ajoute(new Etudiant("Cardigane","Jane"));
 		m14.ajoute(new Etudiant("Dylan", "Bob"));
 		m14.ajoute(new Etudiant("Dylan", "Thomas"));
-
 		System.out.println(m14);
 		
 		
@@ -88,6 +101,15 @@ public class Module {
 		ré afficher les module M14 et M21
 		*/
 		
+		Module m21 = new Module("POO", "M21",20);
+		m21.ajoute(john);
+		m21.ajoute(bob);
+		m21.ajoute(bob);
+		System.out.println(m21);
+		bob.ajouteAbsence();
+		System.out.println(m14);
+		System.out.println(m21);
+			
 	}
 	
 }
